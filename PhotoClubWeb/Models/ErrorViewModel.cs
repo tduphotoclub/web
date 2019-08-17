b89b9ccd6f8b44c58e859981a4d9e8fc5fad8137
falcon <falconhawk@protonmail.com>
2019-08-17 19:38:21 +0900

    initial commit

---
+++ PhotoClubWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhotoClubWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
