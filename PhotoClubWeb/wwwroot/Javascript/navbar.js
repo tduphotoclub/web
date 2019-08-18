c3803f11458cb9fb0eab34f720be17e1836719af
Falcon <falconhawk@protonmail.com>
2019-08-18 14:29:23 +0900

    フッター以外一応全部作った。

---
+++ PhotoClubWeb/wwwroot/Javascript/navbar.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+    function showMobileNavbar() {
+        mobileNavbar.style.width = "250px";
+        mobileNavbar.classList.remove('no-display');
+    }
+
+    function closeNav() {
+        mobileNavbar.style.width = "0px";
+        mobileNavbar.classList.add('no-display')
+    }
+
+    var mobileNavbar = document.getElementById("mobile-navbar");
+    var showButton = document.getElementById('sidebar-show-button');
+    var hideButton = document.getElementById('close-button');
+    showButton.addEventListener('click', showMobileNavbar, true);
+    hideButton.addEventListener('click', closeNav, true);
+}
